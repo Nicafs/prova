150cd81206398cf795b59c4c2d866c02b225882f
Nicollas Santos <nicollas.comp@gmail.com>
2020-08-12 15:54:01 -0400

    Comitando estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ClientDetailsComponent } from './modules/client-details/client-details.component';
+import { UserDetailsComponent } from './modules/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientDetailsComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
